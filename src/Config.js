ac7708fe5edf977b095dc23e871edb211ec75ebc
RuslanMirov <ruslan777mirov@gmail.com>
2021-09-27 11:42:51 +0300

    Update addresses

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 export const API_URL = "http://localhost:9005/api/"
 
 // BSC test Smart chain
-export const NFTAddress = ""
-export const StakeAddress = ""
+export const NFTAddress = "0x1CEa18Ee7985c96e2fCe8aE0578D2cDd0466bDF5"
+export const StakeAddress = "0x15eE7BF458b4dD11855dd3755b501562b0eFc60E"
