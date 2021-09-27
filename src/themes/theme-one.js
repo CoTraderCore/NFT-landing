3d7854c352f220a7591bac9ffcda1143487b48aa
RuslanMirov <ruslan777mirov@gmail.com>
2021-09-27 16:09:09 +0300

    Update theme-one

--- src/themes/theme-one.js
+++ src/themes/theme-one.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 
 import Header from '../components/Header/Header';
-import Hero from '../components/Hero/Hero';
-import Auctions from '../components/Auctions/AuctionsOne';
-import TopSeller from '../components/TopSeller/TopSellerOne';
 import Collections from '../components/Collections/Collections';
-import Explore from '../components/Explore/ExploreOne';
-import Work from '../components/Work/Work';
 import Footer from '../components/Footer/Footer';
 import ModalSearch from '../components/Modal/ModalSearch';
 import ModalMenu from '../components/Modal/ModalMenu';
 import Scrollup from '../components/Scrollup/Scrollup';
+
+// import Work from '../components/Work/Work';
+// import Auctions from '../components/Auctions/AuctionsOne';
+// import TopSeller from '../components/TopSeller/TopSellerOne';
+// import Explore from '../components/Explore/ExploreOne';
+// import Hero from '../components/Hero/Hero';
 
 class ThemeOne extends Component {
     render() {
         return (
             <div className="main">
                 <Header />
-                <Hero />
-                <Auctions />
-                <TopSeller />
                 <Collections />
-                <Explore />
-                <Work />
                 <Footer />
                 <ModalSearch />
                 <ModalMenu />
