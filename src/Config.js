69c3414be1c66c96b0625af81c3708b26518b19f
RuslanMirov <ruslan777mirov@gmail.com>
2021-10-05 10:51:31 +0300

    Update addresses

--- src/Config.js
+++ src/Config.js
@@ -4,5 +4,5 @@ export const API_URL = isLocal ? "http://localhost:9005/api/" : ""
 export const IPFS_IMG_URL = "https://gateway.pinata.cloud/ipfs/QmcEVoN8NWoSoZ4Y4kndirMQS32tVCiTtAF76uE2txkn87/"
 
 // BSC test Smart chain
-export const NFTAddress = "0xaB6d1c1735656C8a2FcCD4C763c14c580f11c755"
-export const StakeAddress = "0xde2982659F430d9fb53F9d95Ab3ff31960555280"
+export const NFTAddress = "0x8EC4F3a6fa30DbCDA59e9AC6Bd3e4826c4991524"
+export const StakeAddress = "0x18eA7B4EeB0b9Ac4Ef070856560124Ac215A7668"
