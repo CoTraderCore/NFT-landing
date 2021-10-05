e89893cdfb5d58d74a422bc01f09ddcb76283089
RuslanMirov <ruslan777mirov@gmail.com>
2021-10-05 10:26:14 +0300

    Update addresses

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,6 @@
-export const API_URL = "http://localhost:9005/api/"
+const isLocal = true
+
+export const API_URL = isLocal ? "http://localhost:9005/api/" : ""
 export const IPFS_IMG_URL = "https://gateway.pinata.cloud/ipfs/QmcEVoN8NWoSoZ4Y4kndirMQS32tVCiTtAF76uE2txkn87/"
 
 // BSC test Smart chain
